7a66f174f5fa4e899cab887978a300d662908746
unknown <ingeniero.nestor.cruz@gmail.com>
2013-08-04 18:03:41 -0500

    NoInit

---
+++ Desarrollo basado en componentes/Felipe-Nestor/Laboratorio1/Lab1-MueblesDeLosAlpes/src/anotaciones/NoInit.java
@@ -0,0 +1,22 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * $Id: NoInit 2013/08/04 GaudiSolutions
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package anotaciones;
+
+import java.lang.annotation.*;
+
+/**
+ *
+ * @author NCRUZ
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={ElementType.TYPE,ElementType.METHOD})
+public @interface NoInit {
+    
+    
+    
+}
