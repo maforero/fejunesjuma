11d114f88f4073da190d938a01f96c5759213897
Felipe Rojas <wfelipe3@gmail.com>
2013-09-21 11:49:27 -0500

    Se actualiza el servicio de persistencia

--- Desarrollo basado en componentes/Felipe-Nestor/laboratorio4/Laboratorio3/Lab3-MueblesDeLosAlpes/Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/ServicioPersistencia.java
+++ Desarrollo basado en componentes/Felipe-Nestor/laboratorio4/Laboratorio3/Lab3-MueblesDeLosAlpes/Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/ServicioPersistencia.java
@@ -63,7 +63,7 @@ public class ServicioPersistencia implements IServicioPersistenciaMockLocal,ISer
     @Override
     public void create(Object obj) throws OperacionInvalidaException
     {
-       //TODO
+       entityManager.persist(obj);
     }
 
     /**
@@ -73,7 +73,7 @@ public class ServicioPersistencia implements IServicioPersistenciaMockLocal,ISer
     @Override
     public void update(Object obj)
     {
-       //TODO
+       entityManager.merge(obj);
     }
 
     /**
@@ -83,8 +83,7 @@ public class ServicioPersistencia implements IServicioPersistenciaMockLocal,ISer
     @Override
     public void delete(Object obj) throws OperacionInvalidaException
     {
-       //TODO
-
+       entityManager.remove(obj);
     }
 
     /**
@@ -95,8 +94,7 @@ public class ServicioPersistencia implements IServicioPersistenciaMockLocal,ISer
     @Override
     public List findAll(Class c)
     {
-        return null;
-        //return entityManager.createQuery("select O from " + c.getSimpleName() + " as O").getResultList();
+       return entityManager.createQuery("select O from " + c.getSimpleName() + " as O").getResultList();
     }
 
     /**
@@ -108,7 +106,6 @@ public class ServicioPersistencia implements IServicioPersistenciaMockLocal,ISer
     @Override
     public Object findById(Class c, Object id)
     {
-        //TODO
-        return null;
+        return entityManager.find(c, id);
     }
 }
